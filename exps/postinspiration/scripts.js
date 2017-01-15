a0331aca6ab51c96095767cce0a0a144744593fa
Max <ryanxam@hotmail.co.uk>
2017-01-15 21:20:39 +0000

    testtables

--- exps/postinspiration/scripts.js
+++ exps/postinspiration/scripts.js
@@ -6,3 +6,12 @@ $('#switch-hitters').sheetrock({
   query: "select A,B,C,D,E,L where E = 'Both' order by L desc",
   fetchSize: 10
 });
+
+
+
+var postInspiration = 'https://docs.google.com/spreadsheets/d/1hoRENWMGoEWdXCTQAwJeSYsayPm1-jbodkt_ZBF1dS0/edit?usp=sharing';
+
+// Load top ten switch hitters.
+$('#posteady').sheetrock({
+  url: mySpreadsheet
+});
