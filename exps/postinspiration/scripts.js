af142f6f01c3e472639d97fe4b235beb4f5d07b6
Max <ryanxam@hotmail.co.uk>
2017-01-15 21:22:37 +0000

    disable

--- exps/postinspiration/scripts.js
+++ exps/postinspiration/scripts.js
@@ -1,11 +1,11 @@
-var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1qT1LyvoAcb0HTsi2rHBltBVpUBumAUzT__rhMvrz5Rk/edit#gid=0';
+// var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1qT1LyvoAcb0HTsi2rHBltBVpUBumAUzT__rhMvrz5Rk/edit#gid=0';
 
-// Load top ten switch hitters.
-$('#switch-hitters').sheetrock({
-  url: mySpreadsheet,
-  query: "select A,B,C,D,E,L where E = 'Both' order by L desc",
-  fetchSize: 10
-});
+// // Load top ten switch hitters.
+// $('#switch-hitters').sheetrock({
+//   url: mySpreadsheet,
+//   query: "select A,B,C,D,E,L where E = 'Both' order by L desc",
+//   fetchSize: 10
+// });
 
 
 
