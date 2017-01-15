a8001909da6a89bd31838c7d7d65f3f207cee842
Max <ryanxam@hotmail.co.uk>
2017-01-15 21:24:22 +0000

    delete

--- exps/postinspiration/scripts.js
+++ exps/postinspiration/scripts.js
@@ -1,11 +1,4 @@
-// var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1qT1LyvoAcb0HTsi2rHBltBVpUBumAUzT__rhMvrz5Rk/edit#gid=0';
 
-// // Load top ten switch hitters.
-// $('#switch-hitters').sheetrock({
-//   url: mySpreadsheet,
-//   query: "select A,B,C,D,E,L where E = 'Both' order by L desc",
-//   fetchSize: 10
-// });
 
 
 
